6b9b98c48f6f80f97323012195a6b424159b0eb8
c4r-mn <cassie@cassie4roseville.com>
2026-07-04 22:22:53 -0500

    Delete config.js

--- config.js
+++
@@ -1,7 +0,0 @@
-// Cassie for Roseville — Field App Config
-// DO NOT COMMIT THIS FILE — add config.js to .gitignore
-window.C4R_CONFIG = {
-  adminPassword: 'rose2026!',
-  teamPassword:  'cassie4roseville',
-  firebaseUrl:   'https://canvas-c4r-default-rtdb.firebaseio.com',
-};
